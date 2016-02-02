b1fa5c1752201d0b00625884117ada179afb75e0
jay <jayloveyun@163.com>
2016-02-02 18:22:28 +0800

    update

--- kolamomo-concurrent/src/main/java/com/kolamomo/concurrent/safety/ThreadSafety.java
+++ kolamomo-concurrent/src/main/java/com/kolamomo/concurrent/safety/ThreadSafety.java
@@ -1,22 +1,81 @@
 package com.kolamomo.concurrent.safety;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 线程安全测试类
  */
 public class ThreadSafety {
-    public void runSafeGernerator() {
-        SafeGernerator safeGernerator = new SafeGernerator();
-        for(int i = 0; i <= 10; i++) {
-            new Thread(safeGernerator).start();
+    public void runSafeSyncGernerator() {
+        List<Thread> threadList = new ArrayList<Thread>();
+        SafeSyncGernerator safeGernerator = new SafeSyncGernerator();
+        for(int i = 0; i < 10000; i++) {
+            Thread t = new Thread(safeGernerator);
+            threadList.add(t);
         }
+
+        long startTime = System.currentTimeMillis();
+        for(Thread t : threadList) {
+            t.start();
+        }
+        for(Thread t : threadList) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(safeGernerator.getValue());
+        System.out.println("cost: " + (System.currentTimeMillis() - startTime));
+    }
+
+    public void runSafeAtomicGernerator() {
+        List<Thread> threadList = new ArrayList<Thread>();
+        SafeAtomicGernerator safeGernerator = new SafeAtomicGernerator();
+        for(int i = 0; i < 10000; i++) {
+            Thread t = new Thread(safeGernerator);
+            threadList.add(t);
+        }
+
+        long startTime = System.currentTimeMillis();
+        for(Thread t : threadList) {
+            t.start();
+        }
+        for(Thread t : threadList) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(safeGernerator.getValue());
+        System.out.println("cost: " + (System.currentTimeMillis() - startTime));
     }
 
 
     public void runUnsafeGernerator() {
+        List<Thread> threadList = new ArrayList<Thread>();
+
         UnsafeGernerator unsafeGernerator = new UnsafeGernerator();
-        for(int i = 0; i <= 10; i++) {
-            new Thread(unsafeGernerator).start();
+        for(int i = 0; i <= 10000; i++) {
+            Thread t = new Thread(unsafeGernerator);
+            threadList.add(t);
         }
+        long startTime = System.currentTimeMillis();
+        for(Thread t : threadList) {
+            t.start();
+        }
+        for(Thread t : threadList) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(unsafeGernerator.getValue());
+        System.out.println("cost: " + (System.currentTimeMillis() - startTime));
     }
 
     /**
@@ -25,19 +84,14 @@ public class ThreadSafety {
     public class UnsafeGernerator implements Runnable {
         private int value = 0;
 
-        public int getNext() {
-            return value++;
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                value++;
+            }
         }
 
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.out.print(getNext() + " ");
+        public int getValue() {
+            return value;
         }
     }
 
@@ -45,37 +99,45 @@ public class ThreadSafety {
      * 线程不安全的value生成器
      * 对++操作进行加锁
      */
-    public class SafeGernerator implements Runnable {
+    public class SafeSyncGernerator implements Runnable {
         private volatile int value = 0;
 
-        public synchronized int getNext() {
-            return value++;
+        public void run() {
+            synchronized(this) {
+                for(int i = 0; i < 10000; i++) {
+                    value++;
+                }
+            }
         }
 
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.out.print(getNext() + " ");
+        public int getValue() {
+            return value;
         }
     }
+
+    public class SafeAtomicGernerator implements Runnable {
+        private AtomicInteger value = new AtomicInteger(0);
+
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                value.getAndIncrement();
+            }
+        }
+
+        public int getValue() {
+            return value.get();
+        }
+    }
+
 
     public static void main(String[] args) {
         ThreadSafety threadSafety = new ThreadSafety();
         System.out.println("unsafe gernerator: ");
         threadSafety.runUnsafeGernerator();
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println();
-        System.out.println("safe gernerator: ");
-        threadSafety.runSafeGernerator();
+        System.out.println("safe synchronized gernerator: ");
+        threadSafety.runSafeSyncGernerator();
+        System.out.println("safe atomic gernerator: ");
+        threadSafety.runSafeAtomicGernerator();
     }
 }
 /*
